7da234bbd8678238276654f83bcb6b41497ca808
mcmul <mcmulle20.laxwi@gmail.com>
2025-02-26 18:07:10 -0600

    added styling to li links

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,7 +4,7 @@ function Navigation() {
   return (
     <nav>
       <ul>
-        <li><NavLink to="/" end>About Me</NavLink></li>
+        <li><NavLink to="/" end>About</NavLink></li>
         <li><NavLink to="/portfolio">Portfolio</NavLink></li>
         <li><NavLink to="/contact">Contact</NavLink></li>
         <li><NavLink to="/resume">Resume</NavLink></li>
